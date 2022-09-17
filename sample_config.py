29eed03514dab2ecac384f27a58b0434e4e54475
pablo2620 <mohamaedm950@gmail.com>
2022-09-18 00:36:11 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -29,7 +29,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Kolkata")
     # set this with required cat repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/jepthoniq/jepthon.git"
+        "UPSTREAM_REPO", "https://github.com/pablo2620/jepthon.git"
     )
 
     # BASIC and MAIN CONFIG VARS
@@ -98,7 +98,7 @@ class Config(object):
     TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "catuserbot")
     # for custom thumb image set this with your required thumb telegraoh link
     THUMB_IMAGE = os.environ.get(
-        "THUMB_IMAGE", "https://telegra.ph/file/ca95524e4734b0d5461b5.jpg"
+        "THUMB_IMAGE", "https://telegra.ph/file/e1de4cd3bee3a4eb5b098.jpg"
     )
     # specify NO_LOAD with plugin names for not loading in jepthon
     NO_LOAD = [x for x in os.environ.get("NO_LOAD", "").split()]
